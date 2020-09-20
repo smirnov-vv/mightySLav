d9afd267e0714eda21c061dadfcb0a87ed67c3be
= <=>
2020-09-20 10:31:59 +0300

    checkbox added

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-class Company {
-  constructor(company, description, type) {
-    this.company = company;
-    this.description = description;
-    this.type = type;
+class SkillsList {
+  constructor(proffesion) {
+    this.proffesion = proffesion;
     this.skills = [];
   }
 
-  getCompany() {
-    return this.company;
+  getFullList() {
+    return this.skills;
   }
 
   getSkill(requestedSkill) {
@@ -18,19 +16,21 @@ class Company {
     return this.skills.filter((item) => item.system === requestedSystem);
   }
 
-  setSkill(skill, system, site, year, description) {
+  setSkill(company, companyDescription, skill, system, site, description, year) {
     this.skills.push({
-      skill, system, site, year, description,
+      company, companyDescription, skill, system, site, description, year
     });
   }
 }
-const skills = {
+
+const skillNames = {
   pnr: 'Выполнение пуско-наладочных работ',
   predesign: 'Подготовка проектной документации',
   design: 'Подготовка рабочей документации',
+  bothdesign: 'Подготовка проектной и рабочей документации',
   asbuilt: 'Подготовка исполнительной документации',
 };
-const systems = {
+const systemNames = {
   aps: 'АПС',
   appz: 'АППЗ',
   soue: 'СОУЭ',
@@ -38,59 +38,67 @@ const systems = {
   cctv: 'СВН',
   scs: 'СКС',
   asutp: 'АСУТП',
+  aovk: 'АОВК',
 };
-const rubezh1 = new Company('ГК Рубеж', 'Производитель оборудования систем безопасности', 'lvs');
-rubezh1.setSkill(skills.asbuilt,
-  systems.aps,
+const companies = {
+  rubezh: { name: 'ГК Рубеж', description: 'Производитель оборудования систем безопасности'},
+  mgd: { name: 'ООО Марин Газ Дизайн', description: 'Комплексное проектирование объектов газовой промышленности' },
+};
+const lvsSkills = new SkillsList('lvs');
+lvsSkills.setSkill(companies.rubezh.name,
+  companies.rubezh.description,
+  skillNames.asbuilt,
+  systemNames.aps,
   'Корпуса проведения саммита АТЭС, остров Русский, Приморский край',
-  2011,
-  '');
-rubezh1.setSkill(skills.design,
-  systems.scs,
+  '',
+  2011);
+lvsSkills.setSkill(companies.rubezh.name,
+  companies.rubezh.description,
+  skillNames.design,
+  systemNames.scs,
   'Завод по производству автокомпонентов на территории ЗАО «Магна Технопласт» индустриального парка «Грабцево», г. Калуга',
-  2009,
-  '');
-
-const mgd = new Company('ООО "Марин Газ Дизайн"', 'Комплексное проектирование объектов газовой промышленности', 'lvs');
-mgd.setSkill(skills.design,
-  systems.asutp,
+  '',
+  2009);
+lvsSkills.setSkill(companies.mgd.name,
+  companies.mgd.description,
+  skillNames.design,
+  systemNames.asutp,
   'Компрессорная станция «Георгиевск» ООО «Газпром трансгаз Ставрополь»',
-  2011,
-  '');
-mgd.setSkill(skills.predesign,
-  systems.aps,
+  '',
+  2011);
+lvsSkills.setSkill(companies.mgd.name,
+  companies.mgd.description,
+  skillNames.bothdesign,
+  systemNames.aps,
+  'Компрессорная станция «Георгиевск» ООО «Газпром трансгаз Ставрополь»',
+  '',
+  2011);
+lvsSkills.setSkill(companies.mgd.name,
+  companies.mgd.description,
+  skillNames.bothdesign,
+  systemNames.soue,
   'Компрессорные станции «Георгиевск» и «Невинномысск» ООО «Газпром трансгаз Ставрополь»',
-  2013,
-  '');
-mgd.setSkill(skills.design,
-  systems.aps,
-  'Компрессорные станции «Георгиевск» и «Невинномысск» ООО «Газпром трансгаз Ставрополь»',
-  2013,
-  '');
-mgd.setSkill(skills.predesign,
-  systems.soue,
-  'Компрессорные станции «Георгиевск» и «Невинномысск» ООО «Газпром трансгаз Ставрополь»',
-  2013,
-  '');
-mgd.setSkill(skills.design,
-  systems.soue,
-  'Компрессорные станции «Георгиевск» и «Невинномысск» ООО «Газпром трансгаз Ставрополь»',
-  2013,
-  '');
+  '',
+  2013);
 
-const filtered = mgd
-  .getSystem(systems.aps)
-  .map((item) => `<a href="#" class="list-group-item list-group-item-action">
+
+const filtered = lvsSkills
+  .getFullList()
+  .map((item) => `
+    <a href="#" class="list-group-item list-group-item-action">
     <div class="d-flex w-100 justify-content-between">
       <h5 class="mb-1">${item.skill}. <b>${item.system}</b></h5>
-      <small>${mgd.getCompany()}, ${item.year}</small>
+      <small>${item.company}, ${item.year}</small>
     </div>
     <p class="mb-1">${item.site}</p>
     <small>Подробнее о ключевых достижениях...</small>
-    </a>`)
+    </a>
+    `)
   .join('');
-// console.log(filtered);
+console.log(filtered);
 
-const data = document.querySelector('.list-group');
-console.log(data);
-data.innerHTML = filtered;
+//const data = document.querySelector('.list-group');
+//console.log(data);
+//data.innerHTML = filtered;
+
+
